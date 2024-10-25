2165786122d0384a043e98e53e4ad7cd36741df7
silverskg <silverskg555@gmail.com>
2024-10-26 01:04:45 +0900

    ♻️ 記事一覧

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,44 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import PostList from "./components/PostList";
-import { posts } from "./data/post";
 import PostDetail from "./components/PostDetail";
+import { useEffect, useState } from "react";
+
+type Post = {
+  id: number;
+  title: string;
+  thumbnailUrl: string;
+  createdAt: string;
+  categories: string[];
+  content: string;
+};
 
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  // 記事一覧を取得useEffect
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts");
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+
+    fetcher();
+  }, []);
+
   return (
-    <>
-      <Router>
-        <Header />
-        <div className="inner">
-          <Routes>
-            {/* 記事一覧のルーティング */}
-            <Route path="/" element={<PostList posts={posts} />} />
-            {/* 記事詳細のルーティング */}
-            <Route path="/posts/:id" element={<PostDetail posts={posts}/>} />
-          </Routes>
-        </div>
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <div className="inner">
+        <Routes>
+          {/* 記事一覧のルーティング */}
+          <Route path="/" element={<PostList posts={posts} />} />
+          {/* 記事詳細のルーティング */}
+          <Route path="/posts/:id" element={<PostDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
