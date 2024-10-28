812af9f8f82ba5425aa0e9e5dbfd3c491e90cc62
silverskg <silverskg555@gmail.com>
2024-10-28 22:47:15 +0900

    ♻️ stateの使い分け（isSubmitting, isSuccess）

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -16,11 +16,11 @@ const ContactForm: React.FC = () => {
     message: "",
   });
 
-  // 送信処理の状態
-  // const [isSubmitting, setIsSubmitting] = useState(false)
+  // 送信中かの状態
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   //送信処理完了の状態
-  const [success, setSuccess] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   // onChangeイベント
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -83,8 +83,11 @@ const ContactForm: React.FC = () => {
       return;
     }
 
-    // setIsSubmitting(true);
-    setSuccess(true); // 送信処理（成功状態）
+    // 送信中の状態をtrueにする
+    setIsSubmitting(true);
+
+    // 送信前には成功メッセージを一旦リセット
+    setIsSuccess(false);
 
     try {
       const response = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/contacts", {
@@ -98,8 +101,14 @@ const ContactForm: React.FC = () => {
 
       // レスポンスのステータスチェック
       if (!response.ok) throw new Error("Network response"); // ネットワークエラーの際エラーを投げる
+
       alert("送信できました");
 
+      // 送信成功の状態をtrueにする
+      // 送信成功時の成功メッセージを表示
+      setIsSuccess(true);
+
+      // input要素ないの文字を消す
       handleClear();
 
       // handleClearを使用するので以下は不要
@@ -114,11 +123,10 @@ const ContactForm: React.FC = () => {
         email: "",
         message: "",
       });
-      
     } catch (error) {
       console.error("Error submitting form:", error);
     } finally {
-      setSuccess(false);
+      setIsSuccess(false);
     }
   };
 
@@ -134,33 +142,33 @@ const ContactForm: React.FC = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>問合わせフォーム</h1>
       <form action="" onSubmit={handleSubmit}>
-        {success && <p style={{ color: "green" }}>メッセージが送信されました。</p>}
+        {isSuccess && <p style={{ color: "green" }}>メッセージが送信されました。</p>}
         <div className={styles.name}>
           <label>名前</label>
           <div className="">
-            <input type="text" value={formData.name} id="name" onChange={handleChange} disabled={success} />
+            <input type="text" value={formData.name} id="name" onChange={handleChange} disabled={isSubmitting} />
             {errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
           </div>
         </div>
         <div className={styles.email}>
           <label>メールアドレス</label>
           <div>
-            <input type="email" value={formData.email} id="email" onChange={handleChange} disabled={success} />
+            <input type="email" value={formData.email} id="email" onChange={handleChange} disabled={isSubmitting} />
             {errors.email && <span style={{ color: "red" }}>{errors.email}</span>}
           </div>
         </div>
         <div className={styles.message}>
           <label>メッセージ</label>
           <div>
-            <textarea value={formData.message} id="message" onChange={handleChange} rows={8} disabled={success} />
+            <textarea value={formData.message} id="message" onChange={handleChange} rows={8} disabled={isSubmitting} />
             {errors.message && <span style={{ color: "red" }}>{errors.message}</span>}
           </div>
         </div>
         <div className={styles.button}>
-          <button type="submit" disabled={success}>
+          <button type="submit" disabled={isSubmitting}>
             送信
           </button>
-          <button type="reset" onClick={handleClear}>
+          <button type="reset" onClick={handleClear} disabled={isSubmitting}>
             クリア
           </button>
         </div>
